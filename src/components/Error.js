dda9ea955dd9933a63203c132ae4b690bcc58d01
israel.micha <israel.micha@intelie.com.br>
2020-11-08 23:26:15 -0300

    fix: error handler

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Button, Result } from 'antd'
 
-const Error = ({ retryAction }) => (
+const Error = () => (
     <Result
     status="500"
     title="500"
     subTitle="Sorry, something went wrong."
-    extra={<Button type="primary" onClick={retryAction}>Try again</Button>}
+    extra={<Button type="primary" onClick={() => window.location.reload()}>Try again</Button>}
   />
 )
 export default Error
