f96606c36a962a14f7e228ee58fc72c4fbd94c83
israel.micha <israel.micha@intelie.com.br>
2020-11-08 23:36:05 -0300

    fix: http => https

--- src/consts/index.js
+++ src/consts/index.js
@@ -1,3 +1,3 @@
-export const FILTERS_ENDPOINT = 'http://www.mocky.io/v2/5a25fade2e0000213aa90776'
+export const FILTERS_ENDPOINT = 'https://www.mocky.io/v2/5a25fade2e0000213aa90776'
 export const FEATURED_PLAYLISTS_ENDPOINT = 'https://api.spotify.com/v1/browse/featured-playlists'
 export const LOGIN_URL = __DEV__ ? 'http://localhost:3000/login' : 'https://isrmicha-spotifood.herokuapp.com/login'
